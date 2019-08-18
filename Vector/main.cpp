9125a9dab6b3cada3cfc3e4cb672a80860686a8a
Iordachescu Anca <48767364+IordachescuAnca@users.noreply.github.com>
2019-08-18 22:49:13 +0300

    Add files via upload

---
+++ Vector/main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include "vector.h"
+
+int main()
+{
+	/*Vector<int> a(3,4);
+	a.push_back(5);
+	a.push_back(10);
+	a.push_back(6);
+
+	a.clear();
+
+	Vector<int> b(4, 10), c(b);
+	a = b;
+	a[0] = 11;
+	a[3] = 9;
+
+	a.insert(0,10);
+	std::cout << a.front() << "\n";
+	b.erase(3);
+	std::cout << b.back() << "\n";
+	
+	std::cout << a.size() << "\n";
+	a.resize(7,3);
+	std::cout << a.size() << "\n";
+	std::cout << b.capacity() << "\n";
+	b.rezerve(10);
+	std::cout << b.capacity() << "\n";
+
+	return 0;*/
+}
